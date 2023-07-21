b5946c98d2d4f332326a967e3cf514c413cacf2d
santiagoaguirr <santiaguirre0@gmail.com>
2023-07-21 16:01:46 -0300

    asd

--- Final LAB 1/Modelos de parcial/VectorProblematico/Funciones.h
+++ Final LAB 1/Modelos de parcial/VectorProblematico/Funciones.h
@@ -7,18 +7,25 @@ bool esVectorProblematico(int vec[], int tam);
 
 bool esVectorProblematico(int vec[], int tam){
     float promedio;
-    int sumatoria = sumarElemVec(vec[], tam);
+    int bandera = 0;
+    int sumatoria;
+    sumatoria = sumarElemVec(vec, tam);
     promedio = sumatoria/tam;
 
     for(int i=0; i<tam-1; i++){
-        if((vec[i+1]- vec[i)>promedio)
+        if(((vec[i+1]- vec[i]))>promedio){
+            bandera = 1;
+        }
     }
-
+    if(bandera == 1)
+        return true;
+    else
+        return false;
 }
 
 int sumarElemVec(int vec[], int tam){
     int suma=0;
-    for(i=0;i<tam;i++){
+    for(int i=0;i<tam;i++){
         suma+=vec[i];
     }
     return suma;
--- Final LAB 1/Modelos de parcial/VectorProblematico/main.cpp
+++ Final LAB 1/Modelos de parcial/VectorProblematico/main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+#include "Funciones.h"
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int v1[4] = { 1, 5, 9, 19 };
+    int v2[4] = {1,3,6,10};
+
+    cout << esVectorProblematico(v1,4) << endl;
+    cout << esVectorProblematico(v2,4) << endl;
+
     return 0;
 }
