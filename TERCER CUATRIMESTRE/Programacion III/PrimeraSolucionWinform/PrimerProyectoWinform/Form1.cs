3c3412ac88191e5a2b7c9868007dccc4dc264762
santiagoaguirr <santiaguirre0@gmail.com>
2024-05-09 15:04:36 -0300

    asd

---
+++ TERCER CUATRIMESTRE/Programacion III/PrimeraSolucionWinform/PrimerProyectoWinform/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerProyectoWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Como te va negrito?");
+           string texto = txtNombre.Text;
+            lblSaludo.Text = "Hola JAVIER";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Bienvenido a mi super aplicación de escritorio!");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
